ef7d08344b5e80e7ebaa73c028619d353a6cee85
BrendanMulcahy <bmm58@case.edu>
2016-07-03 17:44:40 -0400

    Added FeedForward method

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using MathNet.Numerics;
+using MathNet.Numerics.Distributions;
 using MathNet.Numerics.LinearAlgebra;
 
 namespace NeuralNetwork
 {
     public class NeuralNetwork
     {
-        private readonly List<Matrix<double>> _biases = new List<Matrix<double>>();
+        private readonly List<Vector<double>> _biases = new List<Vector<double>>();
         private readonly List<Matrix<double>> _weights = new List<Matrix<double>>();
         private int _layerCount;
         private List<int> _layerSizes;
@@ -19,13 +21,33 @@ namespace NeuralNetwork
 
             foreach (var layerSize in layerSizes.Skip(1))
             {
-                _biases.Add(Matrix<double>.Build.Random(layerSize, 1));
+                _biases.Add(Vector<double>.Build.Random(layerSize, 1));
             }
 
             foreach (var pair in layerSizes.TakeAllButLast().Zip(layerSizes.Skip(1), Tuple.Create))
             {
+                // This might be messed up
                 _weights.Add(Matrix<double>.Build.Random(pair.Item2, pair.Item1));
             }
+        }
+
+        public Vector<double> FeedForward(Vector<double> networkInput)
+        {
+            for (int i = 0; i < _biases.Count; i++)
+            {
+                var bias = _biases[i];
+                var weight = _weights[i];
+
+                networkInput = ApplySigmoid(weight * networkInput + bias);
+            }
+
+            return networkInput;
+        }
+
+        private Vector<double> ApplySigmoid(Vector<double> z)
+        {
+            z.Map(SpecialFunctions.Logistic, z);
+            return z;
         }
     }
 }
