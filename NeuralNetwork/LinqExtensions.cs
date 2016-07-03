eb504e152fd618b2c00ee6c6442bdcdb93d39ccc
BrendanMulcahy <bmm58@case.edu>
2016-07-03 16:57:37 -0400

    Neural network constructor

---
+++ NeuralNetwork/LinqExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace NeuralNetwork
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> TakeAllButLast<T>(this IEnumerable<T> source)
+        {
+            var it = source.GetEnumerator();
+            bool hasRemainingItems;
+            var isFirst = true;
+            var item = default(T);
+
+            do
+            {
+                hasRemainingItems = it.MoveNext();
+                if (hasRemainingItems)
+                {
+                    if (!isFirst) yield return item;
+                    item = it.Current;
+                    isFirst = false;
+                }
+            } while (hasRemainingItems);
+        }
+    }
+}
---
+++ NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace NeuralNetwork
+{
+    public class NeuralNetwork
+    {
+        private readonly List<Matrix<double>> _biases = new List<Matrix<double>>();
+        private readonly List<Matrix<double>> _weights = new List<Matrix<double>>();
+        private int _layerCount;
+        private List<int> _layerSizes;
+
+        public NeuralNetwork(List<int> layerSizes)
+        {
+            _layerCount = layerSizes.Count;
+            _layerSizes = layerSizes;
+
+            foreach (var layerSize in layerSizes.Skip(1))
+            {
+                _biases.Add(Matrix<double>.Build.Random(layerSize, 1));
+            }
+
+            foreach (var pair in layerSizes.TakeAllButLast().Zip(layerSizes.Skip(1), Tuple.Create))
+            {
+                _weights.Add(Matrix<double>.Build.Random(pair.Item2, pair.Item1));
+            }
+        }
+    }
+}
